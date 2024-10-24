1e2b42dc0535d58090b7b5efdcf4ae7e3fe656d7
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-24 16:46:46 +0100

    switch to NavLink components

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function PageNav() {
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
-          <Link to="/pricing">Pricing</Link>
-          <Link to="/product">Product</Link>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/pricing">Pricing</NavLink>
+          <NavLink to="/product">Product</NavLink>
         </li>
       </ul>
     </nav>

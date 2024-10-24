f1aeda5db7b9cfbcfe8d607927acc7d26021335d
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-24 16:03:08 +0100

    add Product to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+
 export default function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }

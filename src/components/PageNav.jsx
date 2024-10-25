b56b7024c404385d4969c2f7599126302130ad69
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-25 13:09:27 +0100

    wrap NavLink components in li tags

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -3,11 +3,15 @@ import styles from "./PageNav.module.css";
 
 export default function PageNav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
           <NavLink to="/pricing">Pricing</NavLink>
+        </li>
+        <li>
           <NavLink to="/product">Product</NavLink>
         </li>
       </ul>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
-import styles from "./Homepage.module.css";
 import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+import styles from "./Homepage.module.css";
 
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
+      <PageNav />
+
       <section>
         <div>
           <h1>

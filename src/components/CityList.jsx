3eff641c9279f013db4fd4216647c1066945f16e
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-27 10:51:06 +0000

    create CityList component

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,5 +1,15 @@
 import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import City from "./City";
 
-export default function CityList() {
-  return <ul className={styles.CityList}>CityList</ul>;
+export default function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  return (
+    <ul className={styles.CityList}>
+      {cities.map((city) => (
+        <City key={city.id} city={city} />
+      ))}
+    </ul>
+  );
 }

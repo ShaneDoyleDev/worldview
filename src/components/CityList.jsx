cec8701c68263bd736873f243f01f462626ef1a7
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-27 11:04:43 +0000

    fix cityList class to correct naming

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -6,7 +6,7 @@ export default function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
   return (
-    <ul className={styles.CityList}>
+    <ul className={styles.cityList}>
       {cities.map((city) => (
         <CityItem key={city.id} city={city} />
       ))}
